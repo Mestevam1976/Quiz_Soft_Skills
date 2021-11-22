76890b2afa004a1f90d58b9f502cb6b01eb743b7
Mestevam1976 <marcio.estevam@yahoo.com>
2021-11-21 22:11:33 -0300

    Incluído função de escolha de cores

--- formatacao.py
+++ formatacao.py
@@ -2,3 +2,16 @@
 def forma_linha():
     print('=' * 75)
     
+def escolher_cor(cor, texto): # função para definir as cores das mensagens
+    if cor == 'red':
+        return f'\033[91m {texto} \033[0m'
+
+    if cor == 'blue':
+        return f'\033[94m {texto} \033[0m'
+
+    if cor == 'yellow':
+        return f'\033[93m {texto} \033[0m'
+
+    if cor == 'green':
+        return f'\033[92m {texto} \033[0m'
+
